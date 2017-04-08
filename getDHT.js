c98fd1cc68b5ccf30840654b50554082fff550eb
Jeremy Heyer <jheyer159@gmail.com>
2017-04-07 21:45:54 -1000

    Test dht

--- getDHT.js
+++ getDHT.js
@@ -1,13 +1,16 @@
 const PythonShell = require('python-shell');
 const pyshell = new PythonShell('./resources/GrovePi/Software/Python/grove_dht_pro.py');
  
-pyshell.on('message', function (message) {
+pyshell.run('message', function (message) {
     // received a message sent from the Python script (a simple "print" statement) 
     console.log(message);
 });
         
 // end the input stream and allow the process to exit 
+// 
+/*
 pyshell.end(function (err) {
     if (err) throw err;
     console.log('DHT closed.');
  });
+*/
