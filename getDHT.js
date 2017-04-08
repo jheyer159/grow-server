e868615fa02855776c344627ea29642e959e9921
Jeremy Heyer <jheyer159@gmail.com>
2017-04-07 21:50:57 -1000

    error handling

--- getDHT.js
+++ getDHT.js
@@ -2,6 +2,7 @@ const PythonShell = require('python-shell');
 const dht = './resources/GrovePi/Software/Python/grove_dht_pro.py'
  
 PythonShell.run( dht, function (message) {
+    if (err) throw err;
     // received a message sent from the Python script (a simple "print" statement) 
     console.log(message);
 });
