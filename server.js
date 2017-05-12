2b323d5fe04661a9ab2ce1085984e3310b883a0e
seanbass <seandbass@gmail.com>
2017-05-11 22:33:44 -0700

    Woops.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.listen("8888", ()=>{
 
 app.get("/", (req, res) => {
     //let dht = require("./getDHT.js")()
-    console.log("dht: ", dht)
+    //console.log("dht: ", dht)
     let temp = getRandomInt( 10, 80 )
     let humd = getRandomInt( 15, 77 )
     let json = {
