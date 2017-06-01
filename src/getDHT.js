221f5f6198c18fb54a9a412a5ecc6b839d3a5520
seanbass <seandbass@gmail.com>
2017-05-31 18:58:02 -0700

    Added lib and source

---
+++ src/getDHT.js
@@ -0,0 +1,26 @@
+var PythonShell = require('python-shell');
+ 
+var options = {
+  mode: 'text',
+  pythonPath: 'path/to/python',
+  pythonOptions: ['-u'],
+  scriptPath: 'path/to/my/scripts',
+  args: ['value1', 'value2', 'value3']
+};
+ 
+var PythonShell = require('python-shell');
+var pyshell = new PythonShell('./GrovePi/Software/Python/grove_dht_pro.py');
+
+
+pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement) 
+  console.log(message);
+});
+ 
+// end the input stream and allow the process to exit 
+pyshell.end(function (err) {
+  if (err) throw err;
+  console.log('finished');
+});
+
+// module.exports = get
