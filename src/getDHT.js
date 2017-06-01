c55510ef391db14dd3dd1d113eb52948e8f49615
Jeremy Heyer <jheyer159@gmail.com>
2017-06-01 09:07:49 -1000

    Updated order of python call

--- src/getDHT.js
+++ src/getDHT.js
@@ -1,10 +1,10 @@
 var PythonShell = require('python-shell');
 //var pyshell = new PythonShell('lib/GrovePi/Software/Python/grove_dht_pro.py');
-var pyshell = new PythonShell('test.py');
 
 export default function (){
   console.log( "exports")
   return new Promise( function(resolve, reject){
+    var pyshell = new PythonShell('test.py');
     pyshell.on('message', function (message) {
       // received a message sent from the Python script (a simple "print" statement) 
       console.log(message)
