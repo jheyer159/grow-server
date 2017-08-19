a3b5e8a10b989cb3b13fc902179ce52865e1b9d1
Jeremy Heyer <jheyer159@gmail.com>
2017-08-18 17:23:11 -0700

    Exports dht

--- src/getDHT.js
+++ src/getDHT.js
@@ -1,6 +1,6 @@
 var PythonShell = require('python-shell')
 
-export default function (){
+module.exports = function (){
   return new Promise( (resolve, reject) => {
     PythonShell.run('lib/GrovePi/Software/Python/grove_dht_pro.py', function (err, results) {
       if (err) reject( err )
