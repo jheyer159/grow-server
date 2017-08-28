73a8aaeaf34545c062dd3b0b6337b204328aeeed
Jeremy Heyer <jheyer159@gmail.com>
2017-08-28 14:49:48 -0700

    no module.exports

--- src/getDHT.js
+++ src/getDHT.js
@@ -27,16 +27,17 @@ var board = new Board({
 
 
 
-
+/*
 module.exports = function (){
     return new Promise( (resolve, reject) => {
     /*PythonShell.run('lib/GrovePi/Software/Python/grove_dht_pro.py', function (err, results) {
       if (err) reject( err )
       resolve( results )
-      */
+      
 
     })
 }
+*/
 
 
 
